ffbee15f3634e62bf55f675a705952b1f288debc
Andrew TOwnsley <atownsley86@gmail.com>
2021-02-20 19:13:25 -0700

    refactor finished, app is functionally complete.  Still needs to be made responsive

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function showError(input, message) {
   // This gets the form-control div by accessing the parent of the input currently being used.  That is because the form-control div is the parent of the input elements...
   // this adds the class of 'error' to form-control...
   const formControl = input.parentElement;
+  console.log(input);
   formControl.className = 'form-control error';
   // Select the small tag within the form-control div,
   // and set it to recieve the message argument using .innerText  
@@ -26,47 +27,86 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase()
-  )};
+  if(re.test(input)) {
+    showSuccess(input);
+  } else {
+    showError(input.value.trim(), 'Email is not valid');
+ };
+}
 
+  // Check required fields...
+  function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+      if(input.value.trim() === '') {
+        showError(input, `${getFieldName(input)} is required`);
+      } else {
+      showSuccess(input);
+      }
+    });
+  }
+// Cehck input length...
+  function checkLength(input, min, max) {
+    if(input.value.length <min) {
+      showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if(input.value.length > max) {
+      showError(input, `${getFieldName(input)} must be less than ${min} characters`);
+    } else 
+    showSuccess(input);
+  }
+
+// Check that the passwords match...
+function checkPasswordsMatch(input1, input2) {
+  if(input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+    } else {
+      showSuccess(input1, input2);
+    }
+  }
+
+// Capitalize first letter of error message...
+  function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  }
 
 // EVENT LISTENERS...
 // Add Event Listener to listen for the FORM SUBMITTING...
 form.addEventListener('submit', function(e) {
   // Prevent Default form submission when submit button is clicked...
   e.preventDefault();
-// Get the value of the username input (username.value)...
-// Check if the username input is blank, if it is, display an error message.
-  if(username.value === '') {
-// The showError funciton takes in (input, message) as arguments...
-    showError(username, 'Username is Required');
-} else {
-  showSuccess(username);
-}
 
-  if(email.value === '') {
-// The showError funciton takes in (input, message) as arguments...
-  showError(email, 'Email is Required'); 
-} else if (!isValidEmail(email.value)) {
-  showError(email, 'Email is not valid');
-} else {
-  showSuccess(email);
-}
-
-  if(password.value === '') {
-// The showError funciton takes in (input, message) as arguments...
-    showError(password, 'Password is Required');
-} else {
-  showSuccess(password);
-}
-
-  if(passwordVerify.value === '') {
-// The showError funciton takes in (input, message) as arguments...
-    showError(passwordVerify, 'Please re-enter password');
-} else {
-  showSuccess(passwordVerify);
-}
-
+  checkRequired([username, email, password, passwordVerify]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, passwordVerify);
 });
+
+
+
+
+
+  // function matchPassword(input) {
+  //   if(password.value !== passwordVerify.value) {
+  //     passwordVerifyError(input, 'Passwords must match');
+  //   } else if 
+  //     (password.value === '' || passwordVerify.value === '') {
+  //     passwordError(input, 'Password is required');
+  //   } else showSuccess(input);
+  // }
+ 
+  // I can't get the 'error' class to apply to the passwordVerify Element when the passwords do not match...
+// function passwordVerifyError(passwordVerify, message) {
+//   const passwordVerifyError = passwordVerify.parentElement;
+//   passwordVerifyError.className = 'form-control error';
+//   const small = passwordVerifyError.querySelector('small');
+//   small.innerText = message;
+// }
+
+// function passwordError(input, message) {
+//   const passwordError = input.parentElement;
+//   passwordError.className = 'form-control error';
+//   const small = passwordError.querySelector('small');
+//   small.innerText = message;
+// }
